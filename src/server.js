298fe86f538e31df695feb589c07ff09d5c42499
Yan Voronkov <sub.voronkov@gmail.com>
2025-09-11 16:13:01 +0300

    Fix server to listen on all interfaces (0.0.0.0)

--- src/server.js
+++ src/server.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 3000; // <--- Получение порта
 
 const server = http.createServer(app); // <--- Создание HTTP сервера на основе app
 
-server.listen(PORT, () => { // <--- **Вот эта часть важна: server.listen()**
+server.listen(PORT, '0.0.0.0', () => { // <--- Слушаем на всех интерфейсах
 	console.log(`Server listening on port ${PORT}`); // <--- Сообщение о запуске сервера
 });
